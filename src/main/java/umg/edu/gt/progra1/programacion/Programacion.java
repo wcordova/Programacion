c7203c48ad3cc06ecd8b9273721898abf0d55cdd
Walter Cordova <wcordova@beesion.com>
2025-03-08 10:03:36 -0600

    Mi primer commit

---
+++ src/main/java/umg/edu/gt/progra1/programacion/Programacion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package umg.edu.gt.progra1.programacion;
+
+/**
+ *
+ * @author wcord
+ */
+public class Programacion {
+
+    public static void main(String[] args) {
+        System.out.println("Hola mundo!");
+    }
+}

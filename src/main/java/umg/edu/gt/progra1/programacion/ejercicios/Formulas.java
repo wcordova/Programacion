21edee1cc7a53f2e84baaa89ac6471c540f20182
Walter Cordova <wcordova@beesion.com>
2025-03-22 10:49:27 -0600

    Tarea: pongamos a prueba lo aprendido

--- src/main/java/umg/edu/gt/progra1/programacion/ejercicios/Formulas.java
+++ src/main/java/umg/edu/gt/progra1/programacion/ejercicios/Formulas.java
@@ -10,4 +10,40 @@ package umg.edu.gt.progra1.programacion.ejercicios;
  */
 public class Formulas {
     
+    /**
+     * Ejercicio: Formula Cuadratica
+     * Resuelva una formula cuadratica
+     * Calcular las racies reales utilizando formula cuadratica
+     * Consideraciones
+     *  Dos soluciones reales si el discrimiantes es = b2 - 4ac es positivo
+     *  Sin soluciones reales si el discriminantre es menor a cero
+     *  Validar los parametros recibidos
+     */
+    
+    /**
+     * Ejercicio: Numero palindrome
+     * Metodo que verifique si un numero es palindromo: Se lee igual de izquierda a derecha y de derecha a izquierda
+     */
+    
+    /**
+     * Ejercicio: Palabra palindrome
+     * Metodo que verifique si una palabra es palindromo: Se lee igual de izquierda a derecha y de derecha a izquierda
+     */
+    
+    /**
+     * Ejercicio: Numero Vampiro con Colmillos
+     * 
+     * Un numero vampiro es un numero entero positivo que puede ser factorizado en dos numeros mas pequeños, llamados colmillos, de tal manera que:
+     *  los colmillos no pueden terminar ambos en cero
+     *  al multiplicar los colmillos el resultado debe ser el numero vampiro
+     *  la contatenacion de los digitos colmillos en cualquier orden, debe coincidir con los digitos del numero vampiro
+     * 
+     * Ejemplo:
+     *  el numero 1260 es un numero vampiro, porque:
+     *  los digitos 21 y 60 juntos (2,1,6,0) pueden reorganizarse para formar el 1260
+     *  sus colmillos son 21 y 60 porque 21 * 60 = 1260
+     * 
+     * El Programa debe encontrar y mostrar el primer numero vampiro dentro de un rango dado.
+     */
+    
 }
